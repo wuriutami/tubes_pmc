7601d9feada3a10fc9c16c29d0fd619ba6470db1
alwan12e3 <31367993+alwan12e3@users.noreply.github.com>
2019-04-05 15:07:42 +0700

    Library global

---
+++ lib.h
@@ -0,0 +1,91 @@
+#ifndef LIB_H
+#define LIB_H
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+
+typedef struct 
+{
+    int count;
+    char id[255];
+}rombongan_t;
+
+typedef struct 
+{
+    char id[255];
+    rombongan_t r[7];
+}prak_t;
+
+typedef struct
+{
+    char id[255];
+    char prak[255];
+    char rom[255];
+}lab_t;
+
+typedef struct 
+{
+    lab_t lab[4];
+    
+}jadwal_t;
+enum hari
+    {Senin, Selasa, Rabu, Kamis, Jumat};
+
+const static struct {
+    enum hari val;
+    const char *str;
+} conversion_hari [] = {
+    {Senin, "Senin"},
+    {Selasa, "Selasa"},
+    {Rabu, "Rabu"},
+    {Kamis, "Kamis"},
+    {Jumat, "Jumat"},
+};
+
+
+enum prak
+    {EL2205, EL2208, EB2200};
+
+const static struct {
+    enum prak      val;
+    const char *str;
+} conversion_prak [] = {
+    {EL2205, "EL2205"},
+    {EL2208, "EL2208"},
+    {EB2200, "EB2200"},
+};
+
+
+enum lab
+    {LAB1, LAB2, LAB3, LSS};
+
+
+const static struct {
+    enum lab val;
+    const char *str;
+} conversion_lab [] = {
+    {LAB1, "LAB1"},
+    {LAB2, "LAB2"},
+    {LAB3, "LAB3"},
+    {LSS, "LSS"},
+};
+
+enum prak str2enum_prak (const char *str);
+enum hari str2enum_hari (const char *str);
+enum prak str2enum_lab (const char *str);
+int syarat_rombongan(int r, char *s);
+
+int input_minggu();
+
+int input_hari(int prak, int s_r);
+
+int input_ruangan(int a, int b);
+
+int max_rom(int prak);
+
+void input_sch(jadwal_t j[][5], prak_t *p);
+
+
+#endif
